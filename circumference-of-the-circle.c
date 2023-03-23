18ba055fb21c550f6cba73ebc166b48d0b867e93
HEYKASHIF <kashifmohd269@gmail.com>
2023-03-24 05:21:43 +0530

    24-march commit

---
+++ circumference-of-the-circle.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    float r;
+    printf("enter the radius of the circle:-");
+    scanf("%f" , &r);
+    printf("circumference of a circle = %f" , 2*3.14*r);
+    return 0;
+}
