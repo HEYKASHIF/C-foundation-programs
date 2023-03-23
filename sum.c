f6baba7a7a078478bfc9fb60db84e7b3cd99a798
HEYKASHIF <kashifmohd269@gmail.com>
2023-03-23 10:50:54 +0530

    second commit

---
+++ sum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("dear student anter two numbers for addition");
+    scanf("%d%d" , &a,&b);
+    sum=(a+b);
+    printf("this is the addition of two numbers, %d" ,sum);
+    return 0;
+}
