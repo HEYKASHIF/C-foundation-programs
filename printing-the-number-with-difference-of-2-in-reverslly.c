7065050b615cedc47d44e48a9ee413b11c452a19
HEYKASHIF <kashifmohd269@gmail.com>
2023-03-26 01:25:19 +0530

    26-march commit5

---
+++ printing-the-number-with-difference-of-2-in-reverslly.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+int n;
+printf("enter a number");
+scanf("%d", &n);
+ for(;n>=2;n=n-2)
+ {
+     printf("these are the numbers %d\n",n);
+     
+ }
+    return 0;
+}
