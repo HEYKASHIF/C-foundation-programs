1d73cbd6444640a596efacc303f265bfc2ef2bc7
HEYKASHIF <kashifmohd269@gmail.com>
2023-03-26 01:13:52 +0530

    26-march commit1

---
+++ prog-to-understand-the-sizeof-operator.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char var[50] ;
+    
+    printf("size of int = %u\n",sizeof(int));
+    printf("size of float = %u\n",sizeof(float) );
+    printf("size of var = %u\n",sizeof(var));
+    printf("size of an integer = %u\n",sizeof(45));
+
+    return 0;
+}
