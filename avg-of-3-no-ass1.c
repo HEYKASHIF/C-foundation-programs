edc91e18280b0a748b7bda53bb15ee3431182b6b
HEYKASHIF <kashifmohd269@gmail.com>
2023-03-29 01:53:36 +0530

    29-march commit1

---
+++ avg-of-3-no-ass1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter three numbers to calculate its average = ");
+    scanf("%d%d%d",&a,&b,&c);
+    printf("this is your average = %d",(a+b+c)/3);
+    return 0;
+}
