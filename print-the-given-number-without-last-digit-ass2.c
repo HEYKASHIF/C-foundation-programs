eb4976ddde39caedc3fec73cdf07cbb3ea67486a
HEYKASHIF <kashifmohd269@gmail.com>
2023-03-29 02:01:14 +0530

    29-march commit10

---
+++ print-the-given-number-without-last-digit-ass2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a number = ");
+    scanf("%d",&n);
+    printf("this is your number without last digit = %d",n/10);
+    return 0;
+    
+}
