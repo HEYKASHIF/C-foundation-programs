fefd22c46042b68486332d624d8dc476aa28fbea
HEYKASHIF <kashifmohd269@gmail.com>
2023-03-29 01:56:33 +0530

    29-march commit4

---
+++ area-of-a-rect-ass1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float l,b;
+    printf("Enter the length and breadth of a rectangle = ");
+    scanf("%f%f",&l,&b);
+    printf("this is your area of a rectangle = %f",l*b);
+    return 0;
+    
+}
