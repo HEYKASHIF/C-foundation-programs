7545d58465143935e57ac8fcbabf3dfc2f377e79
HEYKASHIF <kashifmohd269@gmail.com>
2023-03-29 01:54:46 +0530

    29-march commit2

---
+++ vol-of-cubd-ass1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int l,b,h;
+    printf("Enter the length, breadth and height of a cuboid = ");
+    scanf("%d%d%d",&l,&b,&h);
+    printf("this is your volume of a cuboid = %d",l*b*h);
+    return 0;
+    
+}
