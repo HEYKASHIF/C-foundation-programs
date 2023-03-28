25e74ec0e3e6e8962691cf742724480ed5ea04d6
HEYKASHIF <kashifmohd269@gmail.com>
2023-03-29 01:57:15 +0530

    29-march commit5

---
+++ area-of-circ-ass1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    float r,p=3.14;
+    printf("Enter the radius of a circle = ");
+    scanf("%f",&r);
+    printf("this is your area of a circle = %f",p*r*r);
+    return 0;
+}
