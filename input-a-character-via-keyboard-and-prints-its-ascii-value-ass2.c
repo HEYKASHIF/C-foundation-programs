fa48c08b3ce0838dce8f868b5f4c00a990e0f6b4
HEYKASHIF <kashifmohd269@gmail.com>
2023-03-29 02:02:13 +0530

    29-march commit11

---
+++ input-a-character-via-keyboard-and-prints-its-ascii-value-ass2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char c;
+    printf("press any character to get its ASCII code = ");
+    scanf("%c",&c);
+    printf("ASCII code of %c is %d ",c,c);
+    return 0;
+}
