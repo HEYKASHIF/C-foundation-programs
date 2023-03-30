6847bc4625c66be90bfb536f77b630a6d811794e
HEYKASHIF <kashifmohd269@gmail.com>
2023-03-30 17:43:46 +0530

    30-march commit20

---
+++ a-prog-to-find-greater-between-two-numbers-by-if-else-ass3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter two numbers = ");
+    scanf("%d%d",&a,&b);
+    if(a>b)
+     printf("%d is greater",a);
+    else
+     printf("%d is greater",b);
+    return 0;
+}
