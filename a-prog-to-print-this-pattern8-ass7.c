7f68d38c66cef918aca789e989d98b1258cd3416
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-02 23:29:32 +0530

    2-april commit4

---
+++ a-prog-to-print-this-pattern8-ass7.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,k;
+    for(i=1;i<=4;i++)
+    {
+        k=1;
+        for(j=1;j<=7;j++)
+        {
+            if(j<=3+i&&j>=5-i)
+             {
+                printf("%d",k);
+                j<4?k++:k--;
+             }
+            else
+             printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
