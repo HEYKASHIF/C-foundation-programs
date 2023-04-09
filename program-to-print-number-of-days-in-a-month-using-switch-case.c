709b993bf7d1793f46e9d191b7822857ae04975e
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-10 00:48:08 +0530

    10-april commit8

---
+++ program-to-print-number-of-days-in-a-month-using-switch-case.c
@@ -0,0 +1,52 @@
+// C program to print number of days in a month using switch case.
+#include <stdio.h>
+int main()
+{
+    int month ;
+    printf("Enter month number = ") ;
+    scanf("%d",&month) ;
+    
+    switch(month)
+    {
+        case 1 :
+        printf("31") ;
+        break ;
+        case 2 : 
+        printf("28 or 29") ;
+        break ;
+        case 3 : 
+        printf("31") ;
+        break ;
+        case 4 : 
+        printf("30") ;
+        break ;
+        case 5 : 
+        printf("31") ;
+        break ;
+        case 6 : 
+        printf("30") ;
+        break ;
+        case 7 : 
+        printf("31") ;
+        break ;
+        case 8 : 
+        printf("31") ;
+        break ;
+        case 9 : 
+        printf("30") ;
+        break ;
+        case 10 : 
+        printf("31") ;
+        break ;
+        case 11 : 
+        printf("30") ;
+        break ;
+        case 12 : 
+        printf("31") ;
+        break ;
+        default:
+        printf("Invalid month number") ;
+    }
+
+    return 0;
+}
